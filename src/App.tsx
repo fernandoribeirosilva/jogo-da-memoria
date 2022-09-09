71c417e79ca88a79d5ad0d715099b6e670183625
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-09-09 15:45:52 -0300

    usando o componente InfoImte

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as C from "./App.styles";
 import logoImage from "./assets/devmemory_logo.png";
+import { InfoItem } from "./components/InfoItem/";
 
 function App() {
   return (
@@ -9,7 +10,10 @@ function App() {
           <img src={logoImage} width="200" alt="" />
         </C.LogoLink>
 
-        <C.InfoArea>...</C.InfoArea>
+        <C.InfoArea>
+          <InfoItem label="Tempo" value="00:00" />
+          <InfoItem label="Movimentos" value="0" />
+        </C.InfoArea>
 
         <button>Reiniciar</button>
       </C.Info>
