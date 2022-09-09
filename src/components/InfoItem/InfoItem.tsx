38a3f7f3bc317dcc1a373fb55c35f1991c998457
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-09-09 15:45:16 -0300

    criado item de tempo e dos movimentos e suas estilizações

---
+++ src/components/InfoItem/InfoItem.tsx
@@ -0,0 +1,17 @@
+import * as C from "./style";
+
+type Props = {
+  label: string;
+  value: string;
+};
+
+const InfoItem = ({ label, value }: Props) => {
+  return (
+    <C.Container>
+      <C.Label>{label}</C.Label>
+      <C.Value>{value}</C.Value>
+    </C.Container>
+  );
+};
+
+export { InfoItem };
