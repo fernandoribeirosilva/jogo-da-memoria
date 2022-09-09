b9ef26555282e43e6f05da3eadebcf3a0dca6c77
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-09-08 21:22:35 -0300

    criado o css

---
+++ src/App.styles.ts
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 46.875rem; /* 750px */
+  margin: auto;
+  display: flex;
+  padding: 3.125rem 0; /* 50px 0 */
+
+  @media (max-width: 750px) {
+    flex-direction: column;
+  }
+`;
+
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: auto;
+
+  @media (max-width: 750px) {
+    margin-bottom: 3.125rem; /* 50px */
+    align-items: center;
+  }
+`;
+
+export const LogoLink = styled.a`
+  display: block;
+`;
+
+export const InfoArea = styled.div`
+  width: 100%;
+  margin: 0.625rem 0; /* 10px 0 */
+
+  @media (max-width: 750px) {
+    display: flex;
+    justify-content: space-around;
+    text-align: center;
+  }
+`;
+
+export const GridArea = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+
+  @media (max-width: 750px) {
+    justify-content: center;
+    margin: 0 1.25rem; /* 0 20px */
+  }
+`;
