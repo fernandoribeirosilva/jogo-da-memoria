ea5fe64bc711ce4857d1d61dc86c1b4c4ee333a8
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-09-10 14:33:51 -0300

    remeado este arquivo

---
+++ src/components/InfoItem/InfoItem.style.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 1.25rem 0; /* 20px 0 */
+`;
+
+export const Label = styled.div`
+  font-size: 0.9375rem; /* 15px */
+  color: #6a7d8b;
+`;
+
+export const Value = styled.div`
+  font-size: 2.3125rem; /* 37px */
+  font-weight: bolder;
+  color: #101c40;
+`;
